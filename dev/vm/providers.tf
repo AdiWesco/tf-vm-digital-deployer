732219631e3b7836b36d0911817880e145554ea5
AdiWesco <96870914+AdiWesco@users.noreply.github.com>
2022-02-14 10:50:39 +0530

    updates

--- dev/vm/providers.tf
+++ dev/vm/providers.tf
@@ -5,23 +5,19 @@ terraform {
       version = "=2.91.0"
     }
   }
-<<<<<<< HEAD
-}
-=======
   #backend "remote" {
    # hostname     = "app.terraform.io"
-    #organization = "wescodist"
+   # organization = "wescodist"
 
     #workspaces {
       #name = "devsecops-sandbox"
-      #name = "iot-sandbox"
+    #  name = "iot-sandbox"
       #prefix = "sp-sandbox"
     #}
-  #}
+#  }
 
 }
 provider "azurerm" {
   features {}
   #skip_provider_registration = "true"
 }
->>>>>>> 7944c4c5fe4509ce796f8f19a7bb026e3de16104
